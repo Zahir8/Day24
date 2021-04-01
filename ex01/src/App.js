c0d9a39a0893445819e305eca930b6107962b8fc
Zahir Begulić <46743547+Zahir8@users.noreply.github.com>
2021-04-01 08:41:34 +0200

    updated App.js

--- ex01/src/App.js
+++ ex01/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class App extends Component {
   setMyStorage = () => {
-    document.cookie.setItem("Ywar", 2021);
+    document.cookie = "Year=2021";
     localStorage.setItem("Paragon", "yes, but Arena first");
     sessionStorage.setItem("frontend", "React");
   }
