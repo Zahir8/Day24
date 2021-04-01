0a686e88d07fb92a654725f6056b2634b80c0076
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-04-01 07:52:40 +0200

    cookie-local storage-session storage

---
+++ ex01/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie.setItem("Ywar", 2021);
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  }
+  render() {
+    return(
+      <div className="App">
+        <button onClick={this.setMyStorage}></button>
+      </div>
+    )
+  }
+}
+
+export default App;
