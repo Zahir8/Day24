869e02611bf9401e8b678b48dd85e59928430731
Zahir Begulić <46743547+Zahir8@users.noreply.github.com>
2021-04-01 08:09:04 +0200

    updated App.js

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie.setItem("Ywar", 2021);
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  }
+  
+  getMyStorage = () => {
+    let myCookieData = document.cookie.getItem("Year", 2021);
+    let myLocalStorageData = localStorage.getItem("Paragon", "yes, but Arena first");
+    let mySessionStorageData = sessionStorage.setItem("frontend", "React");
+  }
+  
+  render() {
+    return(
+      <div className="App">
+        <button onClick={this.setMyStorage}></button>
+        <button onClick={this.getMyStorage}></button>
+      </div>
+    )
+  }
 }
 
 export default App;
