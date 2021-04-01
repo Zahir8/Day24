efd71766f1cea2f27d504d07e3e27161b29ee588
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-04-01 09:28:33 +0200

    Get Item

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie="Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  }
+  getCookie = (name) => {
+    
+    var cookieArr = document.cookie.split(";");
+    
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        if(name == cookiePair[0].trim()) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    
+    return null;
+  }
+  
+  getMyStorage = () => {
+    let myCookieData = this.getCookie("Year=2021");
+    let myLocalStorageData = localStorage.getItem("Paragon", "yes, but Arena first");
+    let mySessionStorageData = sessionStorage.setItem("frontend", "React");
+  }
+  
+  render() {
+    return(
+      <div className="App">
+        <button onClick={this.setMyStorage}>Button1</button>
+        <button onClick={this.getMyStorage}>Button2</button>
+      </div>
+    )
+  }
 }
 
 export default App;
